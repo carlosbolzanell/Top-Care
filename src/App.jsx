d7a91a14413ad018f15810420f6ebd3b4913e3be
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-02-26 16:54:00 -0300

    Desenvolvedo componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import Footer from './Components/Footer'
+import UmBotao from './Components/pop-up/UmBotao'
+import TituloLinha from './Components/TituloLinha'
+import Plano from './Components/Plano'
 
 function App() {
 
   return (
-    <>
-      <Footer />
-    </>
+    <div className='flex justify-center items-center h-screen'>
+      <Plano/>
+    </div>
   )
 }
 
---
+++ src/Components/pop-up/UmBotao/UmBotao.jsx
@@ -0,0 +1,9 @@
+const PopupUmBotao = () => {
+    return (
+        <div className="font-poppins flex justify-center items-center flex-col bg-terciaria p-10 w-[28%] rounded-lg">
+            <p>Pet cadastrado com sucesso!</p>
+            <button className="bg-primaria w-[30%] rounded-lg mt-6 p-1">OK</button>
+        </div>
+    )
+}
+export default PopupUmBotao;
