766d4a5f7b6c3cd6b7c43dd525a9f59e632f8fc8
Karoliny Daiana Cieply <karoliny.cieply@gmail.com>
2024-02-29 20:15:01 -0300

    Alteracoes na avaliacao

--- src/Components/Avaliacao/Avaliacao.jsx
+++ src/Components/Avaliacao/Avaliacao.jsx
@@ -4,15 +4,19 @@ import { faStar, } from '@fortawesome/free-solid-svg-icons'
 
 const Avaliacao = () => {
     return (
-        <div className='w-full px-20 font-poppins'>
-            <div className='flex flex-row bg-branco gap-4 shadow-lg p-4 rounded-lg'>
-
-                <div className='pt-2'>
-                    <img className='rounded-full w-[100%]' src="./public/assets/gato.png" />
-                </div>
-                <div className='flex flex-col gap-1'>
-                    <div>
-                        <div className='flex flex-row justify-between py-1'>
+                // div principal
+                <div className='flex flex-row px-20 font-poppins'>
+                <div className='flex flex-row bg-branco gap-2 shadow-md p-4 rounded-lg w-full items-start'>
+                    {/* div da foto */}
+                    <div className=''>
+                        <img className='rounded-full w-[80%]' src="./public/assets/gato.png" />
+                    </div>
+    
+                    {/* div do conteudo */}
+                    <div className='w-full'>
+    
+                        {/* div do nome + estrelas */}
+                        <div className='flex flex-row justify-between py-2'>
                             <p className='pl-2 font-regular text-sm text-preto'>Karoliny Cieply</p>
                             <div className='flex gap-1 items-center'>
                                 <FontAwesomeIcon icon={faStar} style={{ color: "#FFD601", }} />
@@ -22,16 +26,20 @@ const Avaliacao = () => {
                                 <FontAwesomeIcon icon={faStar} style={{ color: "#FFD601", }} />
                             </div>
                         </div>
+                        {/* div da linha */}
+                        <div className='h-[1px] bg-preto w-full'>
+    
+                        </div>
+    
+                        {/* div do texto */}
+                        <div className='conteudo'>
+                            <p className='pl-2 py-2 font-regular text-sm text-preto'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime excepturi odio ea facere vero. Ea, necessitatibus illum laborum nihil labore reprehenderit sed cum, esse itaque, expedita pariatur. Laboriosam, sunt hic.
+                            </p>
+                        </div>
                     </div>
-
-                    <div className='bg-preto h-[1px] w-full'></div>
-                    
-                    <p className='pl-2 py-2 font-regular text-sm text-preto'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in repudiandae omnis autem nostrum deserunt enim, natus, molestias, suscipit vitae quaerat. Harum officiis iusto ullam laudantium minima quibusdam excepturi iure.
-                    </p>
                 </div>
             </div>
-        </div>
     )
 }
 export default Avaliacao;
--- src/Components/EscreverAvaliacao/EscreverAvaliacao.jsx
+++ src/Components/EscreverAvaliacao/EscreverAvaliacao.jsx
@@ -5,35 +5,39 @@ import { faStar, } from '@fortawesome/free-solid-svg-icons'
 const EscreverAvaliacao = () => {
     return (
         // div principal
-        <div className='principal'>
-            {/* div da foto */}
-            <div className=''>
-                <img className='' src="./public/assets/gato.png" />
-            </div>
+        <div className='flex flex-row px-20 font-poppins'>
+            <div className='flex flex-row bg-terciaria gap-2 shadow-lg p-4 rounded-lg w-full items-start'>
+                {/* div da foto */}
+                <div className=''>
+                    <img className='rounded-full w-[80%]' src="./public/assets/gato.png" />
+                </div>
 
-            {/* div do conteudo */}
-            <div className='conteudo'>
+                {/* div do conteudo */}
+                <div className='w-full'>
 
-                {/* div do nome + estrelas */}
-                <div className='nome e estrelas'>
-                    <p className='pl-2 font-regular text-sm text-preto'>Karoliny Cieply</p>
-                    <div className='flex gap-1 items-center'>
-                        <FontAwesomeIcon icon={faStar} style={{ color: "#FFD601", }} />
-                        <FontAwesomeIcon icon={faStar} style={{ color: "#FFD601", }} />
-                        <FontAwesomeIcon icon={faStar} style={{ color: "#FFD601", }} />
-                        <FontAwesomeIcon icon={faStar} style={{ color: "#FFD601", }} />
-                        <FontAwesomeIcon icon={faStar} style={{ color: "#FFD601", }} />
+                    {/* div do nome + estrelas */}
+                    <div className='flex flex-row justify-between py-2'>
+                        <p className='pl-2 font-regular text-sm text-preto'>Karoliny Cieply</p>
+
                     </div>
-                </div>
-                <div className='linha'>
+                    {/* div da linha */}
+                    <div className='h-[1px] bg-preto w-full'>
 
-                </div>
+                    </div>
 
-                {/* div do texto */}
-                <div className='conteudo'>
-                    <p className='pl-2 py-2 font-regular text-sm text-preto'>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut in repudiandae omnis autem nostrum deserunt enim, natus, molestias, suscipit vitae quaerat. Harum officiis iusto ullam laudantium minima quibusdam excepturi iure.
-                    </p>
+                    {/* div do texto */}
+                    <div className='flex flex-col items-center'>
+                        <p className='pl-2 py-2 font-regular text-xs text-cinza-escuro'>
+                            Deixe sua avaliação aqui também!
+                        </p>
+                        <div>
+                            <div>
+                                <input placeholder='Digite sua avaliação' className='font-regular text-xs p-2 rounded-lg'>
+                                </input>
+                            </div>
+                            <a href=''></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
