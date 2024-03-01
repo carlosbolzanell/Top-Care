17cf7c998f3e3979133d779713685c13d96132cf
Carlos Eduardo Bolzanel dos Santos <bolzanel@weg.net>
2024-02-29 21:59:32 -0300

    Projetando componentes

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Footer from './Components/Footer'
 import UmBotao from './Components/pop-up/UmBotao'
 import TituloLinha from './Components/TituloLinha'
 import Plano from './Components/Plano'
+import Select from './Components/Select/Select'
 
 function App() {
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      <Plano/>
+      <Select label={"Selecione o pet"}/>
     </div>
   )
 }
