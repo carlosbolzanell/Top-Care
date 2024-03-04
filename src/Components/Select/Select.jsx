51d6f37139ea7f271086e6cd79b11b3631a5f336
Carlos Bolzanell <carlosbolzanell@gmail.com>
2024-03-04 18:28:52 -0300

    Select

--- src/Components/Select/Select.jsx
+++ src/Components/Select/Select.jsx
@@ -1,17 +1,14 @@
 import { Select, Option } from "@material-tailwind/react";
-import { useState } from "react";
 
 export default function InputSelect({ label, options }) {
-    const [opcoes, setOpcoes] = useState(options || []);
     return (
         <div>
-            <Select label={label}>
+            <Select label={label} className='max-h-xs'>
                 {options.map((opcao, i) => (
                     <Option value={opcao} key={i}>
                         {opcao}
                     </Option>
-                )
-                )}
+                ))}
             </Select>
         </div>
     );
