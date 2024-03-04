16855cdb6a0b2f12551b7fdc703cdbfa1349f47c
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-03-04 16:40:22 -0300

    criando components

--- src/Components/CartõesSalvos/CartoesSalvos.jsx
+++ src/Components/CartõesSalvos/CartoesSalvos.jsx
@@ -1,20 +1,18 @@
 const CartoesSalvos = () => {
     return (
-        <div className="grid grid-cols-2 bg-branco">
-            <div>
+        <div className="bg-branco flex flex-row items-center">
+            <div className="flex flex-col pr-20">
                 <p>Cartão de crédito</p>
                 <p>Kristian Erdmann</p>
             </div>
-            <div>
+            <div className="">
                 <p>Validade 12/28</p>
                 <p>Final 9875</p>
-            </div>
-            <img src="./assets/logo-mastercard.png"></img>
-            <div>
-
-            </div>
+            </div>  
+            <img src="./assets/logo-mastercard.png" className="px-10" alt="Mastercard Logo" />
+            <button className="bg-primaria rounded-full w-8 h-8 font-bold">+</button>
         </div>
-    )
+    );
 }
 
 export default CartoesSalvos;
