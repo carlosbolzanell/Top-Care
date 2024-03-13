bbd4c5e4b0ca40e43a362444e72cec202695765e
Victor Micheluzzi <128429841+VictorTI26@users.noreply.github.com>
2024-03-12 21:55:41 -0300

    Criando components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Footer from './Components/Footer'
+import Footer from './Components/Footer/Footer'
 import UmBotao from './Components/pop-up/UmBotao'
 import TituloLinha from './Components/TituloLinha'
 import Plano from './Components/Plano'
@@ -8,11 +8,12 @@ import HistoricoConsulta from './Components/HistoricoConsulta/HistoricoConsulta'
 import Endereco from './Components/Endereço/Endereco'
 import CartõesSalvos from './Components/CartõesSalvos'
 import Serviços from './Components/Serviços'
+import FotoProduto from './Components/FotoProduto/FotoProduto'
 function App() {
 
   return (
     <div className='flex justify-center items-center h-screen'>
-      <Serviços/>
+      <FotoProduto/>
     </div>
   )
 }
--- src/Components/Serviços/Servicos.jsx
+++ src/Components/Serviços/Servicos.jsx
@@ -4,9 +4,9 @@ const Servicos = () => {
             <div className="flex-auto w-[40%]">
                 <p className="mb-[5%] text-2xl font-bold">Você sabia?</p>
                 <p className="break-normal mr-[8%]">Nós da Top care também trabalhamos com serviços exclusivos e cuidamos do seu pet com todo o amor e carinho. Agende já uma consulta com a gente para seu pet ter os tratamentos que ele merece!</p>
-                <button className="bg-primaria w-[30%] rounded-lg p-2 mt-[5%]">Agende</button>
+                <button className="bg-primaria w-[30%] rounded-lg p-[1%] mt-[5%]">Agende</button>
             </div>
-                <img src="./assets/cachorro_serviços.png" className=""/>
+                <img src="./assets/cachorro_serviços.png" className="w-[41%]"/>
         </div>
     )
 }
